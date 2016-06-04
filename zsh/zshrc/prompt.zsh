e36d5488818d5ff29231e057bf87c411f4885e3d
Yuku TAKAHASHI <taka84u9@gmail.com>
2016-06-04 10:39:25 +0900

    Define color variables as precmd local

--- zsh/zshrc/prompt.zsh
+++ zsh/zshrc/prompt.zsh
@@ -2,29 +2,28 @@ autoload -U colors
 REPORTTIME=3
 colors
 
-# Color
-local DEFAULT=$'%{\e[1;0m%}'
-local RESET="%{${reset_color}%}"
-local GREEN="%{${fg[green]}%}"
-local BLUE="%{${fg[blue]}%}"
-local RED="%{${fg[red]}%}"
-local CYAN="%{${fg[cyan]}%}"
-local YELLOW="%{${fg[yellow]}%}"
-local WHITE="%{${fg[white]}%}"
-local GRAY="%{${fg[gray]}%}"
-local BOLD_GREEN="%{${fg_bold[green]}%}"
-local BOLD_BLUE="%{${fg_bold[blue]}%}"
-local BOLD_RED="%{${fg_bold[red]}%}"
-local BOLD_CYAN="%{${fg_bold[cyan]}%}"
-local BOLD_YELLOW="%{${fg_bold[yellow]}%}"
-local BOLD_WHITE="%{${fg_bold[white]}%}"
-local BOLD_GRAY="%{${fg_bold[gray]}%}"
-
 RPROMPT="%T"
 
 setopt transient_rprompt
 
 function precmd () {
+    # local DEFAULT=$'%{\e[1;0m%}'
+    local RESET="%{${reset_color}%}"
+    # local GREEN="%{${fg[green]}%}"
+    local BLUE="%{${fg[blue]}%}"
+    local RED="%{${fg[red]}%}"
+    local CYAN="%{${fg[cyan]}%}"
+    local YELLOW="%{${fg[yellow]}%}"
+    # local WHITE="%{${fg[white]}%}"
+    # local GRAY="%{${fg[gray]}%}"
+    # local BOLD_GREEN="%{${fg_bold[green]}%}"
+    # local BOLD_BLUE="%{${fg_bold[blue]}%}"
+    local BOLD_RED="%{${fg_bold[red]}%}"
+    # local BOLD_CYAN="%{${fg_bold[cyan]}%}"
+    # local BOLD_YELLOW="%{${fg_bold[yellow]}%}"
+    # local BOLD_WHITE="%{${fg_bold[white]}%}"
+    # local BOLD_GRAY="%{${fg_bold[gray]}%}"
+
     local color
     PROMPT="⚡️ ${YELLOW} %~${RESET} "
     st=`command git status 2>/dev/null`
